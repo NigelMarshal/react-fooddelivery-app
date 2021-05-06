3d89f47b36d9f3c18c4cafb9437f9a3b92cacd80
Nigel Marshal <nigelmarshal@gmail.com>
2021-05-06 09:22:18 +0400

    feat(search): Skeleton structure set up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Search from './components/Search/Search'
 
 function App() {
   return (
     <div className="App">
+      <Search />
       <h1>Let's crack on</h1>
     </div>
   );
---
+++ src/components/Search/Search.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Search = () => {
+    return (
+        <div>
+            <input
+                type='search'
+                placeholder='Search for dishes...'
+            />
+        </div>
+    );
+}
+
+export default Search;
